fd190560e4f65f0d8a8e04f3c9cc1d86fd3e5f35
crystalclearsoldev <crystalclearsoldev@gmail.com>
2025-02-19 03:59:30 -0500

    update dependancies
    Co-authored-by: benjamintan1013 <benjamin1013tan@gmail.com>

--- src/services/jito.rs
+++ src/services/jito.rs
@@ -3,16 +3,13 @@ use solana_sdk::pubkey::Pubkey;
 use std::str::FromStr;
 
 pub async fn init_tip_accounts() -> Result<()> {
-    // For now, just return Ok since we're not using Jito yet
     Ok(())
 }
 
 pub async fn get_tip_account() -> Result<Pubkey> {
-    // Temporary implementation
     Ok(Pubkey::from_str("11111111111111111111111111111111").unwrap())
 }
 
 pub async fn get_tip_value() -> Result<f64> {
-    // Default tip value
     Ok(0.004)
 } 
